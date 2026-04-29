46df9f70216e96c920925eb34ccaada624a8318c
Sir Spunny <lukasjannicke@gmail.com>
2026-04-29 22:36:47 +0200

    removed nvim-treesitter dependencies, changed suffix back to prefix in flake

--- flake.nix
+++ flake.nix
@@ -14,17 +14,7 @@
     flake-utils.lib.eachDefaultSystem (
       system: let
         pkgs = import nixpkgs {inherit system;};
-        # retrieve all available treesitter parsers
-        all-treesitter-parsers-list = builtins.attrValues pkgs.vimPlugins.nvim-treesitter-parsers;
-        valid-treesitter-parsers = builtins.filter (p: pkgs.lib.isDerivation p) all-treesitter-parsers-list;
-        treesitter-parsers = pkgs.symlinkJoin {
-          name = "treesitter-parsers";
-          paths = valid-treesitter-parsers;
-          postBuild = ''
-            mkdir -p $out/parser
-            find -L $out -name "*.so" -exec ln -s {} $out/parser/ \;
-          '';
-        };
+        treesitter-with-grammars = pkgs.vimPlugins.nvim-treesitter.withPlugins (p: builtins.attrValues p);
 
         runtimeDeps = with pkgs; [
           alejandra
@@ -59,7 +49,7 @@
 
           extraMakeWrapperArgs = ''
             --prefix PATH : ${pkgs.lib.makeBinPath runtimeDeps} \
-            --set-default NVIM_TREESITTER_PARSERS "${treesitter-parsers}"
+            --prefix XDG_CONFIG_DIRS : ${treesitter-with-grammars}
           '';
         };
       }
